30d5efc6b697ce0daa08e1881eccd48585b45505
vovabyar <vovabyar@gmail.com>
2022-12-01 22:45:40 +0300

    Thread safe singleton added!

---
+++ src/Singleton/Singleton.java
@@ -0,0 +1,21 @@
+package Singleton;
+
+/* THREAD SAFE Singleton */
+
+public class Singleton {
+    private static Singleton uniqueInstance;
+
+    private Singleton() {
+    }
+
+    public static synchronized Singleton getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+
+    public String getDescription() {
+        return "I'm a thread safe Singleton!";
+    }
+}
